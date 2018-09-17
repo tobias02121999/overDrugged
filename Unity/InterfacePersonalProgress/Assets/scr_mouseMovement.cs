74f26a03d19eb1aa4d7a6427c4a8eb0f603de770
tobias02121999 <hedgefoggames@outlook.com>
2018-09-17 12:58:32 +0200

    Update 0.2

---
+++ Unity/InterfacePersonalProgress/Assets/scr_mouseMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_mouseMovement : MonoBehaviour {
+
+    // Initialize the public variables
+    public float mouseSensitivityX;
+    public float mouseSensitivityY;
+
+    // Initialize the private variables
+    private float speedRight;
+    private float speedUp;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Capture the mouse input and apply it to the current speed
+        speedRight = (Input.GetAxis("Mouse X") * mouseSensitivityX) * Time.deltaTime;
+        speedUp = (Input.GetAxis("Mouse Y") * mouseSensitivityY) * Time.deltaTime;
+
+        // Apply the speed to the current position
+        transform.position += (transform.right * speedRight) + (transform.up * speedUp);
+    }
+}
