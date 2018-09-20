a7283aabed467ff65d97171fb0b6c7106735a02a
tobias02121999 <hedgefoggames@outlook.com>
2018-09-20 12:18:37 +0200

    Zieke schijtzooi
    This reverts commit 934768df80c25aac1ddc397e657d38d2e3755134.

---
+++ Unity/overDrugged/Assets/scr_item.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_item : MonoBehaviour {
+
+    // This script does basically nothing, it is used to detect which child of the player is an item (kinda like the way a tag would function)
+}
