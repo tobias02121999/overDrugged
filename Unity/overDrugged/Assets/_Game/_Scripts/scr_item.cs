5144038fffde38b2e6f32e9093f72ed9f74fa1b3
tobias02121999 <hedgefoggames@outlook.com>
2018-09-20 12:18:47 +0200

    Revert "Zieke schijtzooi"
    This reverts commit a7283aabed467ff65d97171fb0b6c7106735a02a.

---
+++ Unity/overDrugged/Assets/_Game/_Scripts/scr_item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_item : MonoBehaviour {
+
+    // This script does basically nothing, it is used to detect which child of the player is an item (kinda like the way a tag would function)
+
+    // Initialize the public variables
+    public bool isFood;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isFood && collision.gameObject.GetComponent<scr_itemSurface>() != null)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<BoxCollider>().enabled = false;
+            transform.position = collision.gameObject.GetComponent<scr_itemSurface>().itemTransform.position;
+            transform.SetParent(collision.gameObject.GetComponent<scr_itemSurface>().itemTransform);
+        }
+    }
+}
